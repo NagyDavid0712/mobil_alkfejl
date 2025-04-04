bc949278d9acc24323767d313b0122ef26c20442
Nagy Dávid <hdevid12@gmail.com>
2025-04-04 21:19:22 +0200

    Inflation chart placed.

--- app/src/main/java/com/nd/cashflow/CurrencyFragment.java
+++ app/src/main/java/com/nd/cashflow/CurrencyFragment.java
@@ -1,5 +1,6 @@
 package com.nd.cashflow;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,6 +10,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -35,6 +42,33 @@ public class CurrencyFragment extends Fragment {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_dropdown_item, items);
 
         currencyList.setAdapter(adapter);
+
+
+        BarChart chart = view.findViewById(R.id.inflation_chart);
+
+        List<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(1f, 10f));
+        entries.add(new BarEntry(2f, 20f));
+        entries.add(new BarEntry(3f, 15f));
+        entries.add(new BarEntry(4f, 30f));
+
+        BarDataSet dataSet = new BarDataSet(entries, "Eladások");
+        dataSet.setColor(Color.BLUE);
+        dataSet.setValueTextColor(Color.BLACK);
+        dataSet.setValueTextSize(14f);
+
+        BarData barData = new BarData(dataSet);
+        chart.setData(barData);
+
+        chart.getDescription().setEnabled(false);
+        chart.setFitBars(true);
+        chart.animateY(1000);
+
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setDrawGridLines(false);
+
+        chart.invalidate();
 
         return view;
     }
