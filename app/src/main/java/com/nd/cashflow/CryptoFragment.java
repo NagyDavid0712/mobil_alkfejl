a5d7d251a694deaf1dce13209ca5df35a79e51ad
Nagy Dávid <hdevid12@gmail.com>
2025-04-07 20:24:23 +0200

    Base files created. Layout created. Filled with static data. Added event handler.

--- app/src/main/java/com/nd/cashflow/CryptoFragment.java
+++ app/src/main/java/com/nd/cashflow/CryptoFragment.java
@@ -4,61 +4,32 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.Debug;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridLayout;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CryptoFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.nd.cashflow.components.CryptoCard;
+import com.nd.cashflow.handlers.OpenCryptoDescriptionPageEventHandler;
+
 public class CryptoFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public CryptoFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CryptoFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CryptoFragment newInstance(String param1, String param2) {
-        CryptoFragment fragment = new CryptoFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.crypto_fragment, container, false);
+        View view = inflater.inflate(R.layout.crypto_fragment, container, false);
+
+        GridLayout cryptoCardsContainer = view.findViewById(R.id.crypto_cards_container);
+
+        for (int i = 0; i < 20; i++) {
+            CryptoCard card = new CryptoCard(getContext(), "Test" + i, "https://coin-images.coingecko.com/coins/images/1/large/bitcoin.png?1696501400");
+            card.setOnClickListener(new OpenCryptoDescriptionPageEventHandler());
+            cryptoCardsContainer.addView(card);
+
+        }
+
+        return view;
     }
 }
