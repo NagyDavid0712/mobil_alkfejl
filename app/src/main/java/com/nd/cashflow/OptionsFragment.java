53f10505cfec3e0ade707a1bc5c627b2f427696b
Nagy Dávid <hdevid12@gmail.com>
2025-04-08 16:27:21 +0200

    Base files created.

---
+++ app/src/main/java/com/nd/cashflow/OptionsFragment.java
@@ -0,0 +1,24 @@
+package com.nd.cashflow;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class OptionsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.options_fragment, container, false);
+
+
+
+
+
+        return view;
+    }
+}
