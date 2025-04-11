0243791c35446d95a0fe420d0ffd17ee61b1530e
Nagy Dávid <hdevid12@gmail.com>
2025-04-11 17:38:28 +0200

    Login created.

--- app/src/main/java/com/nd/cashflow/LoginActivity.java
+++ app/src/main/java/com/nd/cashflow/LoginActivity.java
@@ -5,14 +5,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.nd.cashflow.modules.CFAuthenticator;
 
 public class LoginActivity extends AppCompatActivity {
 
     private Button loginButton;
     private ImageView switchToRegistrationView;
+
+    private CFAuthenticator cfa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +27,7 @@ public class LoginActivity extends AppCompatActivity {
         loginButton = findViewById(R.id.login_button);
         switchToRegistrationView = findViewById(R.id.switch_to_registration_view);
 
+        cfa = new CFAuthenticator(this);
 
         switchToRegistrationView.setOnClickListener(new ImageView.OnClickListener() {
             @Override
@@ -34,8 +40,13 @@ public class LoginActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(LoginActivity.this, MainActivity.class));
-                finish();
+                TextView email = findViewById(R.id.email_input);
+                TextView password = findViewById(R.id.password_input);
+
+                cfa.loginUser(email.getText().toString(), password.getText().toString());
+
+                //startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                //finish();
             }
         });
 
--- app/src/main/java/com/nd/cashflow/modules/CFAuthenticator.java
+++ app/src/main/java/com/nd/cashflow/modules/CFAuthenticator.java
@@ -1,12 +1,41 @@
 package com.nd.cashflow.modules;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.nd.cashflow.LoginActivity;
+import com.nd.cashflow.MainActivity;
+
 public class CFAuthenticator {
 
-    public static void loginUser(String email, String password) {
-
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private Activity activity;
+    public CFAuthenticator(Activity _activity) {
+        activity = _activity;
     }
 
-    public static void registrateUser() {
+    public void loginUser(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(activity, task -> {
+                   if (task.isSuccessful()) {
+                        FirebaseUser user = mAuth.getCurrentUser();
+
+                       Toast.makeText(null, "Sikeres bejelentkezés!", Toast.LENGTH_SHORT).show();
+
+                        activity.startActivity(new Intent(activity, MainActivity.class));
+                        activity.finish();
+                   } else {
+                       Toast.makeText(null, "Sikertelen bejelentkezés! Hibás e-mail vagy jelszó", Toast.LENGTH_SHORT).show();
+                   }
+                });
+    }
+
+    public void registrateUser() {
 
     }
 
