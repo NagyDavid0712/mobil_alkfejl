b31a1f62f2c12eb45e458aa4d565c857410a4a3b
Nagy Dávid <hdevid12@gmail.com>
2025-04-11 17:14:48 +0200

    Added CFAuthenticator.java.

---
+++ app/src/main/java/com/nd/cashflow/modules/CFAuthenticator.java
@@ -0,0 +1,13 @@
+package com.nd.cashflow.modules;
+
+public class CFAuthenticator {
+
+    public static void loginUser(String email, String password) {
+
+    }
+
+    public static void registrateUser() {
+
+    }
+
+}
