8dea73a5077e9786f62440252fd9cf4c1e23f2c9
Nagy Dávid <hdevid12@gmail.com>
2025-05-18 11:51:00 +0200

    Added Exchangerate api.

--- app/src/main/java/com/nd/cashflow/modules/CFApiWrapper.java
+++ app/src/main/java/com/nd/cashflow/modules/CFApiWrapper.java
@@ -11,9 +11,14 @@ import androidx.annotation.NonNull;
 import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.nd.cashflow.MainActivity;
 import com.nd.cashflow.model.Company;
 
 import com.nd.cashflow.model.Crypto;
+import com.nd.cashflow.model.SimpleExchange;
+import com.nd.cashflow.utils.IsoCodes;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
@@ -29,6 +34,7 @@ import okhttp3.Response;
 
 
 import java.util.AbstractMap.*;
+import java.util.Locale;
 import java.util.Map;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -39,6 +45,7 @@ public class CFApiWrapper {
 
     private String COINGECKO_API_KEY = "CG-XkaYUd8MLTwDHdSsWShbQTUc";
     private String FINNHUB_API_KEY = "d0j196pr01ql09hpgqr0d0j196pr01ql09hpgqrg";
+    private String EXCHANGERATE_API_KEY = "fe4efc054cf330f947659287";
     private Map<String, String> tickers = Map.ofEntries(
             new SimpleEntry<>("AAPL", "apple.com"),
             new SimpleEntry<>("MSFT", "microsoft.com"),
@@ -147,6 +154,45 @@ public class CFApiWrapper {
             });
         }
 
+    }
+
+    public void getExchangeData(CFDataExchangeCallback callback) {
+        Locale locale = Locale.getDefault();
+        String lang = locale.getLanguage();
+        String country = locale.getCountry();
+        Log.d("anyád", country);
+        String iso = IsoCodes.ISO_CODES.get(lang + "_" + country).getIso();
+        String name = IsoCodes.ISO_CODES.get(lang + "_" + country).getName();
+
+        String url = String.format("https://v6.exchangerate-api.com/v6/%s/latest/%s", EXCHANGERATE_API_KEY, iso);
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                callback.onError(e);
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    String responseBody = response.body().string();
+
+                    JsonObject root = new JsonParser().parse(responseBody).getAsJsonObject();
+                    JsonObject rates = root.getAsJsonObject("conversion_rates");
+
+                    double eur = rates.get("EUR").getAsDouble();
+                    double usd = rates.get("USD").getAsDouble();
+
+                    callback.onDataready(new SimpleExchange(iso, name, eur, usd));
+                } else {
+                    callback.onError(new Exception("Hiba"));
+                }
+            }
+        });
     }
 
 }
---
+++ app/src/main/java/com/nd/cashflow/utils/IsoCodes.java
@@ -0,0 +1,35 @@
+package com.nd.cashflow.utils;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class IsoCodes {
+
+    public static class IsoCode {
+        private String iso;
+        private String name;
+        public IsoCode(String iso, String name) {
+            this.iso = iso;
+            this.name = name;
+        }
+
+        public String getIso() {
+            return this.iso;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+    }
+
+    public static Map<String, IsoCode> ISO_CODES = new HashMap<>();
+
+    static {
+        Map<String, IsoCode> map = new HashMap<>();
+
+        map.put("hu_HU", new IsoCode("HUF", "Magyar forint"));
+        map.put("en_US", new IsoCode("USD", "Amerikai dollár"));
+        ISO_CODES = Collections.unmodifiableMap(map);
+    }
+}
